bf40970b52832bf12adc8799ed4194f704a1bff8
Leo Goulart <leogoulart0911@gmail.com>
2023-03-24 19:25:07 -0300

    First Model-Controller-View - Department

---
+++ SalesWebMVC/Controllers/DepartmentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Models;
+
+namespace SalesWebMVC.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> list = new List<Department>();
+            list.Add(new Department { ID = 1, Name = "Eletronics" });
+            list.Add(new Department { ID = 2, Name = "Computers" });
+
+            return View(list);
+        }
+    }
+}
---
+++ SalesWebMVC/Models/Department.cs
@@ -0,0 +1,8 @@
+﻿namespace SalesWebMVC.Models
+{
+    public class Department
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
